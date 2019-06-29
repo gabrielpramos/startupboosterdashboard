c1a992e646d5ca5ff27389f5303975f853441251
gabriel.ramos <ramos.gabriel.pereira@gmail.com>
2019-06-29 18:27:26 -0300

    [Component][Project] Iniciada implementação do componente de campos para pesquisa do repositório e criados reducers para atributos do componente

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,15 @@
-import { CLICK_UPDATE_VALUE } from '../actions/actionTypes';
+import { CLICK_UPDATE_VALUE, CHANGE_USER_NAME, CHANGE_REPOSITORY_NAME } from '../actions/actionTypes';
 export const clickButton = value => ({
   type: CLICK_UPDATE_VALUE,
+  newValue: value
+});
+
+export const userName = value => ({
+  type: CHANGE_USER_NAME,
+  newValue: value
+});
+
+export const repositoryName = value => ({
+  type: CHANGE_REPOSITORY_NAME,
   newValue: value
 });
