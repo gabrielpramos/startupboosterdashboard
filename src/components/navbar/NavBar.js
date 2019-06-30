cdb2f56b744e3c77d0df531b75829a8cf8e93222
gabriel.ramos <ramos.gabriel.pereira@gmail.com>
2019-06-30 15:17:56 -0300

    [Component] NavBar - Ajustado estrutura do css do App e NavBar e adicionado ícone da Liferay

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import { Menu } from 'element-react';
+import icon from '../../assets/icon/liferay-icon-white.svg';
 import './NavBar.css';
 
-class NavBar extends Component {
 
+class NavBar extends Component {
 
     render() {
         return (
             <Menu className="main-navbar" mode="vertical" theme="dark">
                 <Menu.Item index="1" className="home-button">
+                    <img src={icon} alt="liferay icon"></img>
                 </Menu.Item>
             </Menu>
         );
