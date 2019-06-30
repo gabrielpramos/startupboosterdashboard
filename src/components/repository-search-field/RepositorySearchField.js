b455ca059a2df2cff9c60ea7de6b358bbc1985a3
gabriel.ramos <ramos.gabriel.pereira@gmail.com>
2019-06-30 15:26:56 -0300

    [Component] RepositorySearchField - Implementados campos de username e repositoryname, maps para o reduce e eventos

--- src/components/repository-search-field/RepositorySearchField.js
+++ src/components/repository-search-field/RepositorySearchField.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
-import { Card } from 'element-react';
+import { Card, Input } from 'element-react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
 import './RepositorySearchField.css';
+import { bindActionCreators } from 'redux';
+import { userNameChange, repositoryNameChange } from '../../actions';
+
+const mapStateToProps = store => ({
+    userName: store.userNameState.userName,
+    repositoryName: store.repositoryNameState.repositoryName
+})
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ userNameChange, repositoryNameChange }, dispatch);
+
+const enhance = compose(
+    connect(mapStateToProps, mapDispatchToProps)
+);
 
 class RepositorySearchField extends Component {
+
+    state = {
+        userName: '',
+        repositoryName: '',
+    }
+
+    userNameOnChangeHandler = event => {
+        this.setState({
+            userName: event
+        })
+    }
+
+    repositoryNameOnChangeHandler = event => {
+        this.setState({
+            repositoryName: event
+        })
+    }
+
     render() {
+        const {
+            userNameChange,
+            repositoryNameChange,
+        } = this.props;
+
+        const { userName,
+            repositoryName } = this.state;
         return (
             <Card className="box-card">
-                Teste
+                <Input placeholder="Type a username or organization" type="text" name="username-field" className="input-field username-field" title="Github username or organization" value={userName} onChange={this.userNameOnChangeHandler} onBlur={() => userNameChange(userName)} />
+                <Input placeholder="Repository" type="text" name="repositoryname-field" className="input-field repositoryname-field" title="Repository" value={repositoryName} onChange={this.repositoryNameOnChangeHandler} onBlur={() => repositoryNameChange(repositoryName)} />
             </Card>
         );
     }
+
 }
-export default RepositorySearchField;
+
+
+export default enhance(RepositorySearchField);
