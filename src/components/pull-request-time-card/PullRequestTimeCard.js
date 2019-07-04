971f659f33d4300620aae9f1a2b706b3528fdebf
Gabriel Pereira Ramos <gabriel.ramos@mv.com.br>
2019-07-04 15:10:31 -0300

    [Component] PullRequestTimeCard - Implementado envio dos insights do pull request para o componente filho

--- src/components/pull-request-time-card/PullRequestTimeCard.js
+++ src/components/pull-request-time-card/PullRequestTimeCard.js
@@ -1,33 +1,77 @@
 import React, { Component } from 'react';
 import SimpleInfoCard from '../simple-info-card/SimpleInfoCard';
 import { connect } from 'react-redux';
-import { compose } from 'redux';
+import { bindActionCreators, compose } from 'redux';
 import utils from '../../utils/utils';
-import gitapi from '../../services/request';
-import MathUtils from '../../utils/MathUtils';
+import gitapi from '../../services/service';
+import { pullRequestDataInsightsChange } from '../../actions';
+import DateUtils from '../../utils/DateUtils';
 
 const mapStateToProps = store => ({
     userName: store.userNameState.value,
     repositoryName: store.repositoryNameState.value,
     mergeDataInsightsState: store.mergeDataInsightsState.value,
-
 })
 
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ pullRequestDataInsightsChange }, dispatch);
+
 const enhance = compose(
-    connect(mapStateToProps)
+    connect(mapStateToProps, mapDispatchToProps)
 );
 
-const initialState = { cardTitle: 'Average Pull Request Merge Time', value: '' };
+const initialState = {
+    data: {
+        lastCursor: null,
+        totalCount: 0,
+        nodes: []
+    },
+    insights: { average: 0 },
+}
 
 class PullRequestTimeCard extends Component {
 
-    constructor(props) {
-        super(props);
+    constructor() {
+        super();
         this.state = initialState;
     }
 
-    concatNodeArray(nodeArray) {
-        return this.state.data.nodes.concat(nodeArray);
+    nodeFillingCondition() {
+        return this.state.data.nodes.length < this.state.data.totalCount;
+    }
+
+    sumAttributeByName = (obj, acc, curr) => {
+        return (unitStr) => {
+            if (obj.mergeTime) {
+                obj.mergeTime[unitStr] = acc.mergeTime[unitStr] + curr.mergeTime[unitStr];
+            }
+        }
+    }
+    reduceMergeTimeArray = timeUnits => {
+        return (acc, curr) => {
+            let obj = Object.assign({}, acc);
+            if (obj) {
+                timeUnits.forEach(this.sumAttributeByName(obj, acc, curr))
+            }
+            return obj;
+        }
+    }
+
+    insightGeneration = queryChunkedData => {
+        let insights = { average: '' };
+
+        let queryChunkedDataAux = queryChunkedData;
+
+        let timeUnits = Object.keys(queryChunkedData[0].mergeTime);
+
+        let queryData = queryChunkedDataAux.reduce(this.reduceMergeTimeArray(timeUnits));
+        timeUnits.forEach((timeUnit) => {
+            queryData.mergeTime[timeUnit] = Math.round(queryData.mergeTime[timeUnit] / queryChunkedData.length);
+        });
+
+        insights.average = queryData.mergeTime;
+
+        return insights;
     }
 
     fillStateByFetching = (fetchedData) => {
@@ -38,8 +82,7 @@ class PullRequestTimeCard extends Component {
 
             let queryChunkedData = this.state.data.nodes.map((item) => {
                 return {
-                    mergeTime: MathUtils.dateDiff(item.node.mergedAt, item.node.createdAt),
-                    pullRequestSize: (item.node.additions + item.node.deletions)
+                    mergeTime: DateUtils.dateDetailedDiff(item.node.mergedAt, item.node.createdAt),
                 }
             }
             );
@@ -53,7 +96,7 @@ class PullRequestTimeCard extends Component {
 
             this.setState(newState)
 
-            this.props.mergeDataInsightsChange(newState.insights);
+            this.props.pullRequestDataInsightsChange(newState.insights);
         }
     }
 
@@ -63,7 +106,7 @@ class PullRequestTimeCard extends Component {
 
             if (fetchedData && fetchedData.repositories) {
                 let pullRequests = fetchedData.repositories.pullRequests;
-                let nodeArrayConcatenated = utils.concatNodeArray(pullRequests.edges);
+                let nodeArrayConcatenated = utils.concatNodeArray(this.state.data.nodes, pullRequests.edges);
 
                 if (pullRequests.edges.length > 0) {
                     let newState = {
@@ -80,12 +123,13 @@ class PullRequestTimeCard extends Component {
 
             } else {
                 this.setState(initialState);
-                this.props.mergeDataInsightsChange(initialState.insights);
+                this.props.pullRequestDataInsightsChange(initialState.insights);
             }
         });
     }
 
     componentWillReceiveProps(nextProps) {
+
         if (this.props.repositoryName !== nextProps.repositoryName && nextProps.userName && nextProps.repositoryName) {
             let fetchedData = {
                 repositories: null
@@ -99,9 +143,12 @@ class PullRequestTimeCard extends Component {
     }
 
     render() {
+        const title = 'Average Pull Request Merge Time';
+        let average = this.state.insights.average;
+        const cardValue = `${DateUtils.humanizeTime(average.days, 'days', 'day')} ${DateUtils.humanizeTime(average.hours, 'hours', false, 'h')}${DateUtils.humanizeTime(average.minutes, 'minutes', false, 'm')}`;
 
         return (
-            <SimpleInfoCard cardTitle={this.state.cardTitle} cardValue={this.state.value} />
+            <SimpleInfoCard cardTitle={title} cardValue={cardValue} />
         );
     }
 }
